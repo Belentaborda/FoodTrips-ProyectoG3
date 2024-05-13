be45e267e45ec21eed2b79931e452aaad419bf00
Belen Taborda <mbelen.taborda41@gmail.com>
2024-05-13 20:03:11 -0300

    region norte

---
+++ index.js
@@ -0,0 +1,22 @@
+/*const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("Nombre");
+  const email = document.getElementById("Email");
+  const message = document.getElementById("Mensaje");
+  const interest = document.getElementById("Interes");
+  const newsletter = document.getElementById("Newsletter");
+  const profilePic = document.getElementById("Imagen");
+
+  if (!name.value || !email.value || !message.value || !interest.value || !newsletter.checked || !profilePic.value) {
+    alert("Por favor, complete todos los campos obligatorios.");
+    return;
+  }
+
+  form.submit();
+
+  
+});*/
+
